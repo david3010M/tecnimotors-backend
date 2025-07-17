bf60dfa13ea3613e3789987eb9b209ff4a7f7ce3
Miguel Guevara <guevaracajusolmiguel@gmail.com>
2025-07-17 16:10:11 -0500

    feat: setear todos los accesos alos admin

--- database/seeders/AccessSeeder.php
+++ database/seeders/AccessSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Optionmenu;
 use App\Models\TypeUser;
 use Illuminate\Database\Seeder;
 
@@ -14,10 +15,16 @@ class AccessSeeder extends Seeder
      */
     public function run()
     {
-        TypeUser::find(1)->setAccess(1, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,24]);//administrador backend
-        TypeUser::find(2)->setAccess(2, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,24]);//administrador
-        TypeUser::find(3)->setAccess(3, [1, 2, 6, 7, 8, 10]); //cajero
-        TypeUser::find(4)->setAccess(4, [1, 2, 5, 10]); //mecanico
-        TypeUser::find(5)->setAccess(5, [1, 2, 3, 4]);//asesor
+        // TypeUser::find(1)->setAccess(1, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,24]);//administrador backend
+        // TypeUser::find(2)->setAccess(2, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,24]);//administrador
+        // TypeUser::find(3)->setAccess(3, [1, 2, 6, 7, 8, 10]); //cajero
+        // TypeUser::find(4)->setAccess(4, [1, 2, 5, 10]); //mecanico
+        // TypeUser::find(5)->setAccess(5, [1, 2, 3, 4]);//asesor
+
+        $allOptionIds = Optionmenu::pluck('id')->toArray();
+
+        // Asigna todos los accesos al tipo de usuario 1 y 2
+        TypeUser::find(1)?->setAccess(1, $allOptionIds); // Administrador backend
+        TypeUser::find(2)?->setAccess(2, $allOptionIds); // Administrador
     }
 }
